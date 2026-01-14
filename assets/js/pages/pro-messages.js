eb60a1102e3bcccac43cb7a7764fb4cfd8d4088e
delapazjonathan <delapazjonathan@gmail.com>
2026-01-14 14:52:54 -0600

    Delete assets directory

--- assets/js/pages/pro-messages.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", async () => {
-  const me = await window.HPGuards.requirePro();
-  if (!me) return;
-
-  // Later: load threads from /messages?role=pro
-});
